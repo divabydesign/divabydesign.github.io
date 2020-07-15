2edde41855e2e4f7ba99cb2fcdacae35b3a322b8
Artinan <36675457+lifeordeathgame@users.noreply.github.com>
2020-07-15 15:41:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,14 +41,13 @@ function resize() {
   $("#2fbar").css({"top": fbartop + "px"});
   
   let pcontentleft = $("#ncontent").width() + 5;
-  let pcontentwidth = ($("content").width() / 100) * 85 - 5;
-  console.log($("content").width());
+  let pcontentwidth = ($("#content").width() / 100) * 85 - 5;
   $("#pcontent").css({"left": pcontentleft + "px"});
   $("#pcontent").css({"width": pcontentwidth + "px"});
   
   let slogantop = 75 + $("#title").height();
   $("#slogan").css({"top": slogantop + "px"});
   
-  //let imgtop = slogantop + 10 + $("#slogan").height();
-  //$("#himage").css({"top": imgtop + "px"});
+  let imgtop = slogantop + 10 + $("#slogan").height();
+  $("#himage").css({"top": imgtop + "px"});
 }
