596259b1ba79d832d39afe52b9b8282dcc526c40
Artinan <36675457+lifeordeathgame@users.noreply.github.com>
2020-07-14 22:52:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,22 @@ function load() {
 function resize() {
   let navtop = 90 + $("#logo").height();
   $("#nav").css({"top": navtop + "px"});
+  
+  let top = 60 + $("#htext").height();
+  $("#stext").css({"top": top + "px"});
+  
+  top += 60 + $("#stext").height();
+  $("#atext").css({"top": top + "px"});
+  
+  top += 30 + $("#atext").height();
+  $("#1stext").css({"top": top + "px"});
+  
+  top += 30 + $("#1stext").height();
+  $("#2stext").css({"top": top + "px"});
+  
+  top += 30 + $("#2stext").height();
+  $("#3stext").css({"top": top + "px"});
+  
+  top += 60 + $("#3stext").height();
+  $("#ctext").css({"top": top + "px"});
 }
