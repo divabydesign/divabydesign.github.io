8accd3ce468708f79cc9ce9f21121ab415edc1a9
Artinan <36675457+lifeordeathgame@users.noreply.github.com>
2020-07-15 23:21:03 -0400

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,1 +1,47 @@
+function load() {
+  resize();
+}
 
+function resize() {
+  let navtop = 90 + $("#logo").height();
+  $("#nav").css({"top": navtop + "px"});
+  
+  let top = 30 + $("#htext").height();
+  $("#stext").css({"top": top + "px"});
+  
+  top += 15 + $("#stext").height();
+  $("#1stext").css({"top": top + "px"});
+  
+  top += 15 + $("#1stext").height();
+  $("#2stext").css({"top": top + "px"});
+  
+  top += 15 + $("#2stext").height();
+  $("#3stext").css({"top": top + "px"});
+  
+  top += 30 + $("#3stext").height();
+  $("#atext").css({"top": top + "px"});
+  
+  top += 30 + $("#atext").height();
+  $("#ctext").css({"top": top + "px"});
+  
+  let height = 60 + $("#flogo").height();
+  $("#footer").css({"height": height + "px"});
+  
+  let footertop = $("#ncontent").height() - height
+  $("#footer").css({"top": footertop + "px"});
+  
+  let left = ($("#ncontent").width() / 100) * 7.5;
+  $("#1fbar").css({"left": left + "px"});
+  
+  left = ($("#ncontent").width() / 100) * 65;
+  $("#2fbar").css({"left": left + "px"});
+  
+  let fbartop = ($("#footer").height() / 2) - 2.5;
+  $("#1fbar").css({"top": fbartop + "px"});
+  $("#2fbar").css({"top": fbartop + "px"});
+  
+  let pcontentleft = $("#ncontent").width() + 5;
+  let pcontentwidth = ($("#content").width() / 100) * 85 - 5;
+  $("#pcontent").css({"left": pcontentleft + "px"});
+  $("#pcontent").css({"width": pcontentwidth + "px"});
+}
