85e1bfb32e46b4478db4b21c4d0a792eabb26ce2
Artinan <36675457+lifeordeathgame@users.noreply.github.com>
2020-08-14 17:16:08 -0400

    Update shop.js

--- shop.js
+++ shop.js
@@ -4,10 +4,10 @@ function load() {
 
 function resize() {
   let textdivider = ($("#ncontent").height() / 100) * 6;
-  if (textdivider > 25) {
-    textdivider = 25;
+
+  if (textdivider > 55) {
+    textdivider = 55;
   }
-  
   let navtop = 60 + $("#logo").height();
   $("#nav").css({"top": navtop + "px"});
   
@@ -29,8 +29,25 @@ function resize() {
   top += textdivider + $("#atext").height();
   $("#ctext").css({"top": top + "px"});
   
+  let pcontentleft = $("#ncontent").width() + 5;
+  let pcontentwidth = ($("#content").width() / 100) * 85 - 5;
+  $("#pcontent").css({"left": pcontentleft + "px"});
+  $("#pcontent").css({"width": pcontentwidth + "px"});
+  
+  $("#ncontent").css({"width": (($("#content").width() / 100) * 15) + "px"});
+  
+  if ($("#ncontent").width() > 200) {
+    $("#ncontent").css({"width": 200 + "px"});
+    $("#pcontent").css({"width": $("#content").width() - 200 - $("#divider").width() + "px"});
+    $("#pcontent").css({"left": $("#ncontent").width() + $("#divider").width() + "px"});
+  }
+  
   let height = 60 + $("#flogo").height();
   $("#footer").css({"height": height + "px"});
+  
+  $("#ofbar").css({"width": ($("#ncontent").width() / 100) * 27.5 + "px"});
+  $("#tfbar").css({"width": ($("#ncontent").width() / 100) * 27.5 + "px"});
+  $("#flogo").css({"width": ($("#ncontent").width() / 100) * 30 + "px"});
   
   let footertop = $("#ncontent").height() - height;
   $("#footer").css({"top": footertop + "px"});
@@ -44,11 +61,6 @@ function resize() {
   let fbartop = ($("#footer").height() / 2) - 2.5;
   $("#ofbar").css({"top": fbartop + "px"});
   $("#tfbar").css({"top": fbartop + "px"});
-  
-  let pcontentleft = $("#ncontent").width() + 5;
-  let pcontentwidth = ($("#content").width() / 100) * 85 - 5;
-  $("#pcontent").css({"left": pcontentleft + "px"});
-  $("#pcontent").css({"width": pcontentwidth + "px"});
   
   let shopcontainerwidth = $("#pcontent").width() - 120;
   let shopcontainerheighte = $("#pcontent").height() - 120 - $("#ehead").height();
@@ -91,4 +103,8 @@ function resize() {
   newstylesheet.html('.shoptileimg{height:' + tileimgsize + 'px}');
   //newstylesheet.html('.shoptileimg{width:' + tileimgsize + 'px}');
   
+  $("#htext").css({"left": (($("#ncontent").width() - $("#htext").width()) / 2) + "px"});
+  $("#stext").css({"left": (($("#ncontent").width() - $("#stext").width()) / 2) + "px"});
+  $("#atext").css({"left": (($("#ncontent").width() - $("#atext").width()) / 2) + "px"});
+  $("#ctext").css({"left": (($("#ncontent").width() - $("#ctext").width()) / 2) + "px"});
 }
